20c805492407c4fbfde7327011a3035f8e41fb24
Mark Mayfield <mmayfield@panzura.com>
2020-04-14 09:15:20 -0500

    Adding most updated version

--- o365beat/README.md
+++ o365beat/README.md
@@ -1,4 +1,4 @@
-# Welcome to O365beat 7.2.0
+# Welcome to O365beat 7.5.1
 
 Shipper for Office 365 logs from Management Activities API.
 
@@ -15,13 +15,13 @@ instance. To load the dashboards for O365beat into Kibana, run:
     ./o365beat setup -e
 
 For further steps visit the
-[Getting started](https://www.elastic.co/guide/en/beats/o365beat/7.2/o365beat-getting-started.html) guide.
+[Getting started](https://www.elastic.co/guide/en/beats/o365beat/7.5/o365beat-getting-started.html) guide.
 
 ## Documentation
 
-Visit [Elastic.co Docs](https://www.elastic.co/guide/en/beats/o365beat/7.2/index.html)
+Visit [Elastic.co Docs](https://www.elastic.co/guide/en/beats/o365beat/7.5/index.html)
 for the full O365beat documentation.
 
 ## Release notes
 
-https://www.elastic.co/guide/en/beats/libbeat/7.2/release-notes-7.2.0.html
+https://www.elastic.co/guide/en/beats/libbeat/7.5/release-notes-7.5.1.html
--- o365beat/install-service-o365beat.ps1
+++ o365beat/install-service-o365beat.ps1
@@ -11,10 +11,10 @@ $workdir = Split-Path $MyInvocation.MyCommand.Path
 # Create the new service.
 New-Service -name o365beat `
   -displayName O365beat `
-  -binaryPathName "`"$workdir\o365beat.exe`" -c `"$workdir\o365beat.yml`" -path.home `"$workdir`" -path.data `"C:\ProgramData\o365beat`" -path.logs `"C:\ProgramData\o365beat\logs`""
+  -binaryPathName "`"$workdir\o365beat.exe`" -c `"$workdir\o365beat.yml`" -path.home `"$workdir`" -path.data `"C:\ProgramData\o365beat`" -path.logs `"C:\ProgramData\o365beat\logs`" -E logging.files.redirect_stderr=true"
 
 # Attempt to set the service to delayed start using sc config.
 Try {
-  Start-Process -FilePath sc.exe -ArgumentList 'config o365beat start=delayed-auto'
+  Start-Process -FilePath sc.exe -ArgumentList 'config o365beat start= delayed-auto'
 }
-Catch { Write-Host "An error occured setting the service to delayed start." -ForegroundColor Red }
+Catch { Write-Host -f red "An error occured setting the service to delayed start." }
